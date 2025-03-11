feb1ba889cd60f44252b1b5e605f6fe804beffac
waqfs <mainmdev@gmail.com>
2025-03-11 10:28:50 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+// Add any interactivity or dynamic behavior here
+
+// Example: Log a message when the page loads
+window.onload = function() {
+  console.log("Welcome to My Projects Page!");
+};
